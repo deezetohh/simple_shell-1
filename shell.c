82a40993c04b958f09b7a4aefb29800859467311
ussyneh <usmanhassanish@gmail.com>
2023-05-16 03:22:07 -0700

    string compare function done

--- shell.c
+++ shell.c
@@ -19,9 +19,23 @@ void _putchar(char c)
 }
 
 /**
+ * _strcmp - string comparison
+ * @s1: first argument
+ * @s2: second argument
+ * Return: greater, less or zero based on input
  */
-int _strcmp(const char *str1, const char *str2)
+int _strcmp(char *s1, char *s2)
 {
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
 
 /**
