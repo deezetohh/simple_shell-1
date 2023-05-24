d06db2bc6d0578245e6a68e087992526324f975d
NqobileMdlalose <mdlalose150@gmail.com>
2023-05-24 00:22:03 -0700

    fixes

--- shell.c
+++ shell.c
@@ -17,7 +17,7 @@ void print_env_var(void)
 	while (*env != NULL)
 	{
 		write(STDOUT_FILENO, *env, _strlen(*env));
-		write(STDOUT_FILENO, "\n", stdout);
+		write(STDOUT_FILENO, "\n", 1);
 		env++;
 	}
 }
