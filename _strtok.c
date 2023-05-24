39fe0a5d3bd9625d57c5bc2494ae634f72a967a7
NqobileMdlalose <mdlalose150@gmail.com>
2023-05-24 08:43:19 -0700

    fixes

--- _strtok.c
+++ _strtok.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stddef.h>
+#include <string.h>
 
 /**
  * _strtok - Tokenizes a string based on given delimiters.
@@ -12,31 +13,41 @@
  */
 char *_strtok(char *str, const char *delim)
 {
-	static char *next_token;
+	static char *next;
 	char *start;
 	const char *delimiter;
 
 	if (str != NULL)
-		next_token = str;
-	if (next_token == NULL || *next_token == '\0')
+		next = str;
+	if (next == NULL || *next == '\0')
 		return (NULL);
-
-	start = next_token;
-
-	while (*next_token != '\0')
+	while (*next != '\0' && (*next == ' ' || strchr(delim, *next) != NULL))
+	{
+		if (*next == ' ')
+			next++;
+		else
+			*next++ = '\0';
+	}
+	if (*next == '\0')
+	{
+		next = NULL;
+		return (NULL);
+	}
+	start = next;
+	while (*next != '\0')
 	{
 		delimiter = delim;
 		while (*delimiter != '\0')
 		{
-			if (*next_token == *delimiter)
+			if (*next == *delimiter)
 			{
-				*next_token = '\0';
-				next_token++;
+				*next = '\0';
+				next++;
 				return (start);
 			}
 			delimiter++;
 		}
-		next_token++;
+		next++;
 	}
 	return (start);
 
