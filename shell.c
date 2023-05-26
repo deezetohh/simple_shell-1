94557df54150693efd4cb3b1948b6a8af67461d2
ussyneh <usmanhassanish@gmail.com>
2023-05-26 07:28:18 -0700

    fix betty

--- shell.c
+++ shell.c
@@ -59,7 +59,7 @@ void execute_command(char *args[], char *err_msg)
 {
 	pid_t pid;
 	int status;
-	
+
 	if (_strcmp(args[0], "ls") == 0)
 		args[0] = "/bin/ls";
 	if (access(args[0], F_OK) == -1)
@@ -135,7 +135,7 @@ void run_non_interactive_mode(int argc, char **argv)
 			print_env_var();
 		if (_strcmp(args[0], "exit") == 0 && argc == 2)
 			exit_command(args);
-		if (argc == 1 && (_strcmp(args[0], "env") == 0) && (_strcmp(args[0], "exit") != 0))
+		if (argc == 1 && (_strcmp(args[0], "env") == 0))
 			execute_command(args, err_msg);
 		if (argc > 1)
 			handle_arguments(args, err_msg);
@@ -172,9 +172,9 @@ void run_interactive_mode(int argc, char **argv)
 		parse_input(line, args, &argc);
 		if (_strcmp(args[0], "env") == 0)
 			print_env_var();
-		if (_strcmp(args[0], "exit") == 0 && argc ==2)
+		if (_strcmp(args[0], "exit") == 0 && argc == 2)
 			exit_command(args);
-		if (argc == 1 && (_strcmp(args[0], "env") != 0) && (_strcmp(args[0], "exit") != 0))
+		if (argc == 1 && (_strcmp(args[0], "env") != 0))
 			execute_command(args, err_msg);
 		if (argc > 1)
 			handle_arguments(args, err_msg);
